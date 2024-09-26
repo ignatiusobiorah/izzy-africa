f9a2df4ba60453b9fe12d75ebf12bf5af384c345
ignatiusobiorah <iobiorah23@gmail.com>
2024-09-26 13:13:21 +0100

    Add styles to events-category

--- src/components/EventCat.js
+++ src/components/EventCat.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import artCulture from "../images/activity-8122959_640.jpg";
+import community from "../images/charity-8366471_640.png";
+import foodDrinks from "../images/drink-462776_640.jpg";
+import sports from "../images/skateboard-4030508_640.jpg";
+import music from "../images/audience-1853662_640.jpg";
+import career from "../images/job-5382501_640.jpg";
 const EventCat = () => {
   return (
-    <div>
+    <div className='events-category'>
         <div className='event-cat-text'>
             <h2>
                 There is something here for everyone
@@ -13,6 +18,30 @@ const EventCat = () => {
             </p>
         </div>
         <div className='event-cat-icons'>
+          <div className='icons'>
+            <img src= {artCulture} alt='spray-painting'/>
+            <p>Arts & Culture</p>
+          </div>
+          <div className='icons'>
+            <img src= {sports} alt='skateboarder'/> 
+            <p>Sports</p>
+          </div>
+          <div className='icons'>
+            <img src= {community} alt='hands-together'/>
+            <p>Community</p>
+          </div>
+          <div className='icons'>
+            <img src= {foodDrinks} alt='drink'/>
+            <p>Food & Drinks</p>
+          </div>
+          <div className='icons'>
+            <img src= {career} alt='job'/>
+            <p>Career & Business</p>
+          </div>
+          <div className='icons'>
+            <img src= {music} alt='audience'/>
+            <p>Concerts & Music</p>
+          </div>
 
         </div>
     </div>
